c018df4814e1d8ebdca85d694043d1228060ef75
Vitaliy Kinakh <vitaliy.kinakh@unige.ch>
2021-03-18 23:04:32 +0100

    Implement augmentation classes

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -3,3 +3,4 @@ from src.data.cifar_20 import CIFAR20
 from src.data.dataset_wrapper import UnsupervisedDatasetWrapper, SimCLRDataTransform
 from src.data.jigsaw_dataset import JigsawDataset
 from src.data.rotation_dataset import RotationDataset
+from src.data.augmentor import ContrastiveAugmentor, ValidAugmentor, PatchAugmentor
---
+++ src/data/augmentor.py
@@ -0,0 +1,116 @@
+from typing import Tuple
+
+import PIL
+
+import torch
+import torchvision.transforms as transforms
+
+from .datasets import DATASET_STATS, SUPPORTED_DATASETS
+from .gaussian_blur import GaussianBlur
+
+
+class SimCLRDataTransform:
+    def __init__(self, transform: transforms.Compose):
+        self.transform = transform
+
+    def __call__(self, sample: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        xi = self.transform(sample)
+        xj = self.transform(sample)
+        return xi, xj
+
+
+class ContrastiveAugmentor:
+
+    """Applies augmentation for contrastive learning"""
+
+    def __init__(self, dataset: str, input_size: Tuple[int, int, int]):
+        """
+        Args:
+            dataset: dataset to apply augmentations to
+
+            input_size: input image size
+
+        Raises:
+            ValueError: if specified dataset is unsupported
+        """
+
+        if dataset not in SUPPORTED_DATASETS:
+            raise ValueError('Unsupported dataset')
+
+        stats = DATASET_STATS[dataset]
+
+        h, w = input_size[:2]
+        size = (h, w)
+        blur_kernel_size = 2 * int(.05 * h) + 1
+        color = transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
+
+        augmentations = transforms.Compose([
+            transforms.Resize(size, interpolation=PIL.Image.LANCZOS),
+            transforms.RandomResizedCrop(size=size, interpolation=PIL.Image.LANCZOS),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomApply([color], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            GaussianBlur(kernel_size=blur_kernel_size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=stats['mean'], std=stats['std'])
+        ])
+        self._augmentations = SimCLRDataTransform(augmentations)
+
+    def __call__(self, sample: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self._augmentations(sample)
+
+
+class ValidAugmentor:
+
+    """Applies augmentation for validation and testing"""
+
+    def __init__(self, dataset: str, input_size: Tuple[int, int, int]):
+        """
+        Args:
+            dataset: dataset to apply augmentations to
+
+            input_size: input image size
+
+        Raises:
+            ValueError: if specified dataset is unsupported
+        """
+
+        if dataset not in SUPPORTED_DATASETS:
+            raise ValueError('Unsupported dataset')
+
+        stats = DATASET_STATS[dataset]
+
+        h, w = input_size[:2]
+        size = (h, w)
+
+        self._augmentations = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize(size=size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=stats['mean'], std=stats['std'])
+        ])
+
+    def __call__(self, sample: torch.Tensor) -> torch.Tensor:
+        return self._augmentations(sample)
+
+
+class PatchAugmentor:
+
+    """Applies augmentations to patch"""
+
+    def __init__(self, input_size: Tuple[int, int, int]):
+        """
+        Args:
+            input_size: input image size
+        """
+
+        h, w = input_size[:2]
+        size = (h, w)
+
+        self._augmentations = transforms.Compose([
+            transforms.Resize(size=size),
+            transforms.ToTensor()
+        ])
+
+    def __call__(self, sample: torch.Tensor) -> torch.Tensor:
+        return self._augmentations(sample)
