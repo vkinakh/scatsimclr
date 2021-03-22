44c823b2597f0665af7c215d97b8ae45c1fc459c
Vitaliy Kinakh <vitaliy.kinakh@unige.ch>
2021-03-22 13:03:51 +0100

    Implement BaseDatasetWrapper

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,8 +1,9 @@
 from src.data.gaussian_blur import GaussianBlur
 from src.data.cifar_20 import CIFAR20
-from src.data.dataset_wrapper import UnsupervisedDatasetWrapper
 from src.data.jigsaw_dataset import JigsawDataset
 from src.data.rotation_dataset import RotationDataset
 from src.data.augmentor import ContrastiveAugmentor, ValidAugmentor, PatchAugmentor
-from src.data.embedding_extractor import EmbeddingExtractor, EmbeddingType
+from src.data.base_dataset_wrapper import BaseDatasetWrapper
+from src.data.dataset_wrapper import UnsupervisedDatasetWrapper
 from src.data.pretext_task_dataset_wrapper import PretextTaskDatasetWrapper
+from src.data.embedding_extractor import EmbeddingExtractor, EmbeddingType
---
+++ src/data/base_dataset_wrapper.py
@@ -0,0 +1,54 @@
+from typing import Tuple
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.sampler import SubsetRandomSampler
+
+from .datasets import SUPPORTED_DATASETS
+
+
+class BaseDatasetWrapper(ABC):
+
+    def __init__(self,
+                 batch_size: int,
+                 valid_size: float,
+                 input_size: Tuple[int, int, int],
+                 dataset: str):
+
+        if dataset not in SUPPORTED_DATASETS:
+            raise ValueError('Unsupported dataset')
+
+        if len(input_size) != 3:
+            raise ValueError('Input size should be in form (H, W, C)')
+
+        if batch_size <= 0:
+            raise ValueError('Incorrect `batch_size` value. It should be positive')
+
+        if valid_size <= 0 or valid_size >= 1:
+            raise ValueError('Incorrect `valid_size`. It should be in range (0, 1)')
+
+        self._batch_size = batch_size
+        self._valid_size = valid_size
+        self._input_size = input_size
+        self._dataset = self.get_dataset(dataset)
+
+    def get_data_loaders(self) -> Tuple[DataLoader, DataLoader]:
+        n = len(self._dataset)
+        indices = list(range(n))
+        np.random.shuffle(indices)
+
+        split = int(np.floor(self._valid_size * n))
+        train_idx, valid_idx = indices[split:], indices[:split]
+
+        train_sampler = SubsetRandomSampler(train_idx)
+        valid_sampler = SubsetRandomSampler(valid_idx)
+
+        train_loader = DataLoader(self._dataset, batch_size=self._batch_size, sampler=train_sampler)
+        valid_loader = DataLoader(self._dataset, batch_size=self._batch_size, sampler=valid_sampler)
+        return train_loader, valid_loader
+
+    @abstractmethod
+    def get_dataset(self, dataset: str) -> Dataset:
+        pass
