f5d9562f607a7bf6591091f47d4a93689ddddf54
Vitaliy Kinakh <vitaliy.kinakh@unige.ch>
2021-03-23 12:04:52 +0100

    Add running scripts

---
+++ main.py
@@ -0,0 +1,21 @@
+import yaml
+
+from src.trainer import ScatSimCLRTrainer, PretextTaskTrainer
+
+
+def run_unsupervised():
+    config = yaml.load(open('./config.yaml', 'r'), Loader=yaml.FullLoader)
+
+    trainer = ScatSimCLRTrainer(config)
+    trainer.train()
+
+
+def run_pretext():
+    config = yaml.load(open('./config_pretext.yaml', 'r'), Loader=yaml.FullLoader)
+
+    trainer = PretextTaskTrainer(config)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    run_pretext()
